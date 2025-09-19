3459f3ba20f650ba38fcdaa80423c5ea0c34c89a
toyboot4e <toyboot4e@gmail.com>
2025-09-20 00:36:48 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Repro
+
+Run the following commands to start VM :
+
+```sh
+$ nixos-rebuild build-vm --flake .#test && ./result/bin/run-test-vm
+```
+
+Log in with user name `test`.
+
---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "plover-flake #263 repro";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    plover-flake.url = "github:openstenoproject/plover-flake";
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+      home-manager,
+      plover-flake,
+      ...
+    }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      nixosConfigurations.test = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          {
+            system.stateVersion = "25.11";
+
+            networking.hostName = "test";
+            users.users.test = {
+              isNormalUser = true;
+              password = "";
+              extraGroups = [
+                "dialout"
+                "input"
+              ];
+            };
+          }
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.users.test =
+              { pkgs, ... }:
+              {
+                imports = [ inputs.plover-flake.homeManagerModules.plover ];
+                home.stateVersion = "25.11";
+                programs.plover = {
+                  enable = true;
+                  package = plover-flake.packages.${pkgs.system}.plover-full;
+                  settings = {
+                    "Plugins" = {
+                      enabled_extensions = [
+                        "plover_lapwing_aio"
+                      ];
+                    };
+                    "System" = {
+                      name = "Lapwing";
+                    };
+                  };
+                };
+              };
+          }
+        ];
+
+      };
+    };
+}
